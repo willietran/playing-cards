98edc28da7ef1e19f02b7032d503f586a0119abd
Willie Tran <tran.william26@gmail.com>
2014-08-06 13:42:50 -0700

    test_settings.py applied

---
+++ war/war/test_settings.py
@@ -0,0 +1,14 @@
+__author__ = 'WillieTran'
+
+import os
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "test",
+        "USER": os.environ.get('PG_USER'),
+        "PASSWORD": os.environ.get('PG_PASSWORD'),
+        "HOST": "127.0.0.1",
+        "PORT": "",
+    }
+}
